acd11fafdf6a4571ac1b7d57b0fb30db1a139f08
ogilvie1231 <ogilvie1231@gmail.com>
2020-01-25 19:03:58 -0700

    Updates

---
+++ precache-manifest.579dfa8ec6a5454689102dd86dbac54b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf5d61666909e1a7c3ebd930e3acff18",
+    "url": "/clicky/index.html"
+  },
+  {
+    "revision": "ec2e053a1c806506678b",
+    "url": "/clicky/static/css/main.787ebc62.chunk.css"
+  },
+  {
+    "revision": "7c6f1bc1ec97b4b6bd64",
+    "url": "/clicky/static/js/2.b2dc5bac.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky/static/js/2.b2dc5bac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ec2e053a1c806506678b",
+    "url": "/clicky/static/js/main.6a7c7af1.chunk.js"
+  },
+  {
+    "revision": "fd9db7a444932b3dca9a",
+    "url": "/clicky/static/js/runtime-main.0567bb31.js"
+  }
+]);
