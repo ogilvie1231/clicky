ab7389224350885db6b60c7db28769e465ab5bf4
ogilvie1231 <ogilvie1231@gmail.com>
2020-03-05 08:27:32 -0700

    updating instructions and readme

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,17 @@
 import React from "react";
-import "./header.css"; 
+import "./header.css";
 
 function HeaderDisp() {
-    return  (
-<div className="headerDiv">
-    <h1>Clicky Game!</h1>
-    <h3>Click an image below to get started!</h3> 
-</div>
-)
+  return (
+    <div className="headerDiv">
+      <h1>Clicky Game!</h1>
+      <h3>
+        Click an image to earn a point. Your target score is 12 but if you click
+        the same image twice, you lose! Click an image below to get started and
+        remeber, don't click the same image twice!
+      </h3>
+    </div>
+  );
 }
 
-export default HeaderDisp
+export default HeaderDisp;
